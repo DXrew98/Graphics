a1a0a5d9fc6e5a9568f1e5917c5bb91200f82d42
Andrew C Ward <andrew.ward@students.aie.edu.au>
2016-09-08 17:30:35 -0700

    working on triangles

--- RenderUtils/cRenderUtils.h
+++ RenderUtils/cRenderUtils.h
@@ -1,1 +1,11 @@
-void test();
+
+struct vertex {
+	float position[4] = { 0, 0, 0, 1 };
+};
+
+struct Geometry {
+	
+	unsigned vao, vbo, ibo, size;
+};
+
+Geometry makeGeometry(const Vertex * verts. )
